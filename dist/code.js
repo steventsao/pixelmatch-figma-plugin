fca50edc15fd09faf3b3986e6c1a1952cec153f3
Steven Tsao <steventsao@me.com>
2023-09-21 14:05:15 -0700

    add esbuild

--- dist/code.js
+++ dist/code.js
@@ -1,22 +1,20 @@
-const init = () => {
+(() => {
+  // code.ts
+  var init = () => {
     figma.showUI(__html__, { width: 800, height: 600 });
-    // Pre-render the selection
     const currentSelection = figma.currentPage.selection[0];
-    currentSelection
-        .exportAsync({ format: "PNG", constraint: { type: "SCALE", value: 2 } })
-        .then((data) => {
-        figma.ui.postMessage({ img: data, type: "selection-data" });
+    currentSelection.exportAsync({ format: "PNG", constraint: { type: "SCALE", value: 2 } }).then((data) => {
+      figma.ui.postMessage({ img: data, type: "selection-data" });
     });
     figma.ui.onmessage = (message) => {
-        if (message["img-added"]) {
-            const currentSelection = figma.currentPage.selection[0];
-            currentSelection.exportAsync({ format: "PNG" }).then((data) => {
-                /** exported */
-            });
-        }
-        else if (message.type === "quit") {
-            figma.closePlugin();
-        }
+      if (message["img-added"]) {
+        const currentSelection2 = figma.currentPage.selection[0];
+        currentSelection2.exportAsync({ format: "PNG" }).then((data) => {
+        });
+      } else if (message.type === "quit") {
+        figma.closePlugin();
+      }
     };
-};
-init();
+  };
+  init();
+})();
